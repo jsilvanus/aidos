b6e64c8a31928aa38ef93bfbac5a4401dc43f3fc
Juha Itäleino <jsilvanus@gmail.com>
2026-08-17 10:54:06 +0300

    Add Android target to downloads module

--- engine/downloads/build.gradle.kts
+++ engine/downloads/build.gradle.kts
@@ -2,18 +2,21 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     kotlin("multiplatform")
+    id("com.android.library")
 }
 
 kotlin {
     jvm {
         compilerOptions.jvmTarget.set(JvmTarget.JVM_21)
     }
+    androidTarget()
 
     sourceSets {
         val commonMain by getting
         val commonTest by getting
         val jvmMain by getting
         val jvmTest by getting
+        val androidMain by getting
 
         commonMain.dependencies {
             implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
@@ -24,5 +27,17 @@ kotlin {
         jvmTest.dependencies {
             implementation(kotlin("test-junit5"))
         }
+        androidMain.dependencies {
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+        }
+    }
+}
+
+android {
+    namespace = "dev.aidos.downloads"
+    compileSdk = 35
+
+    defaultConfig {
+        minSdk = 26
     }
 }
