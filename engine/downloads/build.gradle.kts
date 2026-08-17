5df45ccca684d3489b7fa1607d70f29de28a9a1b
Juha Itäleino <jsilvanus@gmail.com>
2026-08-17 10:50:32 +0300

    Configure downloads JVM target for HTTP implementation

--- engine/downloads/build.gradle.kts
+++ engine/downloads/build.gradle.kts
@@ -1,21 +1,28 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
 plugins {
     kotlin("multiplatform")
 }
 
 kotlin {
-    jvmToolchain(21)
-    jvm()
+    jvm {
+        compilerOptions.jvmTarget.set(JvmTarget.JVM_21)
+    }
 
     sourceSets {
-        commonMain.dependencies {
-            // TODO(RFC-0103): This dependency on :kernel violates RFC-0103's requirement.
-            // Refactor during RFC-0103 implementation.
-            implementation(project(":kernel"))
-            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-        }
+        val commonMain by getting
+        val commonTest by getting
+        val jvmMain by getting
+        val jvmTest by getting
 
-        jvmTest.dependencies {
+        commonMain.dependencies {
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+        }
+        commonTest.dependencies {
             implementation(kotlin("test"))
+        }
+        jvmTest.dependencies {
+            implementation(kotlin("test-junit5"))
         }
     }
 }
